7801faf25f8240dc51d47630c3c74c4e3dac168c
elias-kodehode <elias96.kodehode@gmail.com>
2026-02-17 15:01:13 +0100

    Styling fixes and reformatting, tictactoe

--- js/basicRouter.js
+++ js/basicRouter.js
@@ -1,11 +1,11 @@
-const pages = ["Index","Side1", "Side2"];
+const pages = ["Index","Side1", "Side2", "API", "tictactoe"];
 
 var navBar = document.getElementById("nav-bar");
 var header = document.getElementsByTagName("header")[0];
 
 window.onload = function(){
 
-    if(pages.length > 0){
+    if(navBar != null && pages.length > 0){
 
         //dynamically fill the nav element with anchor tags, based on the pages array
         for (let index = 0; index < pages.length; index++) {
@@ -18,7 +18,7 @@ window.onload = function(){
 
     //Automatically set the header to the name of the page
     if(header != null && header.childElementCount <= 0){
-        var h1 = this.document.createElement("h1");
+        var h1 = document.createElement("h1");
         header.appendChild(h1);
         var fileName = window.location.href.split("/").slice(-1).toString().split(".")[0];
         h1.textContent = capitalizeFirstLetter(fileName);
@@ -26,6 +26,7 @@ window.onload = function(){
 
 }
 
+//Create anchor tag and set its href to name, and display said name
 function createLinkElement(name) {
     var a = document.createElement("a");
     a.setAttribute("href", name.toString().toLowerCase() + ".html");
@@ -33,6 +34,7 @@ function createLinkElement(name) {
     return a;
 }
 
+//capitalizes first letter of a string
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
